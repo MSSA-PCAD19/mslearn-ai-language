01f5b9991c0b8f6fba7c927f028410cd4f4fc045
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2025-10-01 15:52:50 -0700

    start with lab1

---
+++ Labfiles/01-analyze-text/csharp/text-analysis/text-analysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+string endpoint = config["endpoint"];
+string key = config["key"];
+
+var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+Response<DetectedLanguage> result = client.DetectLanguage(File.ReadAllText(@"reviews\review2.txt"));
+Console.WriteLine($"Language detected is : {result.Value.Name}, confidence: {result.Value.ConfidenceScore}");
