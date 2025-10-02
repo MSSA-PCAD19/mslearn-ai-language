0f5e02f2ecff54f04e7d04a66f5ccdfe3e4b8c75
abonillajr <anthony.bonilla.pcad19@msftmssa.ommicrosoft.com>
2025-10-02 07:44:30 -0700

    Practice with AI Models over Azure

---
+++ Labfiles/01-analyze-text/CSharp/text-analysis/text-analysis/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+string endpoint = config["endpoint"];
+string key = config["key"];
+string readFile = @"reviews\review1.txt";
+
+var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+Response<DetectedLanguage> result = client.DetectLanguage(File.ReadAllText(readFile));
+Console.WriteLine($"The language detected is : {result.Value.Name}, confidence: {result.Value.ConfidenceScore}");
+
+Response<DocumentSentiment> result2 = client.AnalyzeSentiment(File.ReadAllText(readFile));
+Console.WriteLine($"\nSentiment: {result2.Value.Sentiment}\n");
+
+var keyPhrases = client.ExtractKeyPhrases(File.ReadAllText(readFile));
+
+foreach (var item in keyPhrases.Value)
+{
+    Console.WriteLine($"Key Phase: {item}");
+}
