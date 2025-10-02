6e73f102ed02bb231e85354342f56867f85a1be6
abonillajr <anthony.bonilla.pcad19@msftmssa.ommicrosoft.com>
2025-10-02 07:45:01 -0700

    leftover

--- Labfiles/01-analyze-text/CSharp/text-analysis/text-analysis/Program.cs
+++ Labfiles/01-analyze-text/CSharp/text-analysis/text-analysis/Program.cs
@@ -5,19 +5,35 @@ using Microsoft.Extensions.Configuration;
 var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 string endpoint = config["endpoint"];
 string key = config["key"];
-string readFile = @"reviews\review1.txt";
+string fileName = @"reviews\review4.txt";
 
+Console.WriteLine($"======Language Detection=======");
 var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(key));
 
-Response<DetectedLanguage> result = client.DetectLanguage(File.ReadAllText(readFile));
-Console.WriteLine($"The language detected is : {result.Value.Name}, confidence: {result.Value.ConfidenceScore}");
+Response<DetectedLanguage> result = client.DetectLanguage(File.ReadAllText(fileName));
+Console.WriteLine($"Language detected is : {result.Value.Name}, confidence: {result.Value.ConfidenceScore}");
 
-Response<DocumentSentiment> result2 = client.AnalyzeSentiment(File.ReadAllText(readFile));
-Console.WriteLine($"\nSentiment: {result2.Value.Sentiment}\n");
+Console.WriteLine($"======Sentiment================");
+Response<DocumentSentiment> resultSentiment= client.AnalyzeSentiment(File.ReadAllText(fileName));
+Console.WriteLine($"Sentiment is : {resultSentiment.Value.Sentiment}");
+Console.WriteLine($"""
+ Sentiment Confidence
+    negative:{resultSentiment.Value.ConfidenceScores.Negative}
+    neutral:{resultSentiment.Value.ConfidenceScores.Neutral}
+    positive:{resultSentiment.Value.ConfidenceScores.Positive}
+ """);
 
-var keyPhrases = client.ExtractKeyPhrases(File.ReadAllText(readFile));
-
-foreach (var item in keyPhrases.Value)
+Console.WriteLine($"======Key Phrases=======");
+var responseKeyPhrases = client.ExtractKeyPhrases(File.ReadAllText(fileName));
+foreach (var item in responseKeyPhrases.Value)
 {
-    Console.WriteLine($"Key Phase: {item}");
+    Console.WriteLine(item);
 }
+
+Console.WriteLine($"======Recognize Entities=======");
+var responseEntity = client.RecognizeEntities(File.ReadAllText(fileName));
+foreach (var item in responseEntity.Value)
+{
+    Console.WriteLine($"{item.Category}:{item.Text}");
+}
+
